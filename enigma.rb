3fe23590487e79a508013901f1a8e6d460345fa6
Keith Dingle <kdingle88@yahoo.com>
2020-03-16 18:38:25 -0500

    add Enigma encrypt instance method

--- enigma.rb
+++ enigma.rb
@@ -1,11 +1,54 @@
-class Enigma
-    def encrypt(message, key, date)    
-      puts ' I am encrypt'
-    end
-    
-    def decrypt(ciphertext, key, date)
-      puts 'I am decrypt'
+require_relative 'calculate.rb' 
+
+class Enigma 
+  def encrypt(message, key = Calculate.random_number_string, date = Calculate.format_date)
+
+    #Do error checking raise errors if message contains non a-z
+    raise ArgumentError if !message.match?(/^[a-zA-Z\s]*$/)
+
+    encrypt_chars = Calculate.character_set
+
+    char_offsets = Calculate.offsets(date)
+
+    key_hash = Calculate.keys(key)
+
+    char_shifts = Calculate.shift(key_hash, char_offsets)
+
+    message_array = message.downcase.split('')
+
+    encrypt_message = message_array.map.with_index  do |char,index| 
+    if index % 4 ==0
+      new_index = char_shifts[:a_shift] + encrypt_chars.index(char)
+
+      encrypt_chars[(new_index % 27)]
+
+    elsif index % 4 ==1
+      new_index = char_shifts[:b_shift] + encrypt_chars.index(char)
+
+      encrypt_chars[(new_index % 27)]
+
+    elsif index % 4 ==2
+      new_index = char_shifts[:c_shift] + encrypt_chars.index(char)
+
+      encrypt_chars[(new_index % 27)]
+
+    elsif index % 4 ==3
+      new_index = char_shifts[:d_shift] + encrypt_chars.index(char)
+
+      encrypt_chars[(new_index % 27)]
     end
   end
 
+    { 
+      encryption: encrypt_message.join,
+      key: key,
+      date: date
+    }
+    
+  end
+end
+
+enigma = Enigma.new
+
+puts enigma.encrypt('hello world')
 
--- enigma_spec.rb
+++ enigma_spec.rb
@@ -1,3 +1,21 @@
 require 'rspec/autorun'
+require_relative 'calculate.rb'
 require_relative 'enigma'
 
+describe Enigma do
+  describe '#encrypt' do
+    it 'returns hash containing encryption, key, and date' do
+      enigma = Enigma.new
+      
+      expect(enigma.encrypt("hello world", "02715", "040895")).to include({encryption: "keder ohulw", key: "02715", date: "040895"})
+    end
+    it 'raises error if message contains non a-z or space charaters' do
+      enigma = Enigma.new
+
+      
+      expect{enigma.encrypt("he11o world", "02715", "040895")}.to raise_error(ArgumentError)
+    end
+    
+      
+  end 
+end 
