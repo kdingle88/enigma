c60f34e2a25d5084eb85b7806b735b6ed955faef
Keith Dingle <kdingle88@yahoo.com>
2020-03-18 13:42:20 -0500

    refactor calculate methods

--- lib/calculate.rb
+++ lib/calculate.rb
@@ -7,45 +7,28 @@ module Calculate
     end
 
     def format_date(date = Date.today.strftime("%m%d%y"))
-
       valid_date?(date) ? date : (raise ArgumentError)
     end
-    
+
     def offsets(formatted_date)
-      date_offsets = {
-      a_offset:0,
-      b_offset:0,
-      c_offset:0,
-      d_offset:0
-    }
-
-      date_squared = formatted_date.to_i**2
-
-      last_four_digits = date_squared.to_s.split('').last(4).join.to_i.digits
-
-      date_offsets[:a_offset] = last_four_digits[3]
-      date_offsets[:b_offset] = last_four_digits[2]
-      date_offsets[:c_offset] = last_four_digits[1]
-      date_offsets[:d_offset] = last_four_digits[0]
-
-      date_offsets
+      last_four_digits = offset_digits(formatted_date)
+  
+      {
+        a_offset: last_four_digits[3],
+        b_offset: last_four_digits[2],
+        c_offset: last_four_digits[1],
+        d_offset: last_four_digits[0]
+      }
     end
 
     def keys(num_string = random_number_string)
-      digit_keys = {
-        a_key:'',
-        b_key:'',
-        c_key:'',
-        d_key:'' }
+      num_list = num_sets(num_string)
 
-      num_set = num_string.split('').each_cons(2).map(&:join)
-
-      digit_keys[:a_key] = num_set[0]
-      digit_keys[:b_key] = num_set[1]
-      digit_keys[:c_key] = num_set[2]
-      digit_keys[:d_key] = num_set[3]
-
-      digit_keys
+      { a_key: num_list[0],
+        b_key: num_list[1],
+        c_key: num_list[2],
+        d_key: num_list[3]
+      }
     end
 
     def random_number_string
@@ -57,23 +40,38 @@ module Calculate
 
       number_string
     end
+    
 
     def shift(key_hash,offset_hash)
-      shift_hash = {
+      {
         a_shift: key_hash[:a_key].to_i + offset_hash[:a_offset],
         b_shift: key_hash[:b_key].to_i  + offset_hash[:b_offset],
         c_shift: key_hash[:c_key].to_i  + offset_hash[:c_offset],
         d_shift: key_hash[:d_key].to_i  + offset_hash[:d_offset],
       }
-      shift_hash
+      
     end
-  end
+  
 
-  private
+    private
 
-  def self.valid_date?(date)
-    date.match?(/^\d+$/) && date.length == 6
+    def valid_date?(date)
+      date.match?(/^\d+$/) && date.length == 6
+    
 
+    end
+
+    def offset_digits(formatted_date)
+      date_squared = formatted_date.to_i**2
+
+      date_squared.to_s.split('').last(4).join.to_i.digits
+    end
+
+    def num_sets(num_string)
+      num_string.split('').each_cons(2).map(&:join)
+    end
+
+    
   end
 end
 
