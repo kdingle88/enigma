f393c74348ca61186ac5c34057d9c4c1845a3562
Keith Dingle <kdingle88@yahoo.com>
2020-03-23 14:22:45 -0500

    spec refactored to make easier to parse. Create local variables for params

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -6,20 +6,32 @@ RSpec.describe Enigma do
   describe '#encrypt' do
     context 'contains params message, key, and date' do
       it 'returns hash with message, key, and date' do
-        expect(subject.encrypt("hello world", "02715", "040895")).to include({encryption: "keder ohulw", key: "02715", date: "040895"})
+        msg = "hello world"
+        key = "02715"
+        date ="040895"
+
+        expect(subject.encrypt(msg, key, date)).to include({encryption: "keder ohulw", key: key, date: date})
       end
     end
 
     context 'when message contains non a-z or space charaters' do
       it 'raises error' do
-        expect{subject.encrypt("he11o world", "02715", "040895")}.to raise_error(ArgumentError)
+        msg = "he11o world"
+        key = "02715"
+        date ="040895"
+
+        expect{subject.encrypt(msg, key, date)}.to raise_error(ArgumentError)
       end
     end
   end
 
   describe '#decrypt' do
     it 'returns hash containing encrypted message, key, and date' do
-      expect(subject.decrypt("keder ohulw","02715", "040895")).to include({decryption: "hello world", key: "02715", date: "040895"})
+      msg = "keder ohulw"
+      key = "02715"
+      date ="040895"
+
+      expect(subject.decrypt(msg,key,date)).to include({decryption: "hello world", key: key, date: date})
     end
     context 'when message contains non a-z or space charaters' do
       it 'raises error' do
@@ -30,7 +42,10 @@ RSpec.describe Enigma do
   describe '#crack' do 
     context 'contains params encrypted message and date' do
       it 'returns hash containing decripted message, key, and date' do
-        expect(subject.crack("vjqtbeaweqihssi","291018")).to include({decryption: "hello world end",date: "291018",key: "08304"})
+        msg = "vjqtbeaweqihssi"
+        date ="291018"
+        
+        expect(subject.crack(msg,date)).to include({decryption: "hello world end",date: date,key: "08304"})
       end
     end
   end 
